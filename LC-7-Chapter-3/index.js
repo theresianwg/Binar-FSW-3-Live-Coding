4fdaa7a4c679f8c7040e8628a586114b27c9f233
theresianwg <theresianawangsih@gmail.com>
2023-03-20 21:40:55 +0700

    20/03/2023

---
+++ LC-7-Chapter-3/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello There")
+//         alert('Hello FSW3');
+//         function hello() {
+//             console.log('Hello World')
+//         }
+
+document.querySelector("h1").innerHTML="Yah, Good Bye )):";
